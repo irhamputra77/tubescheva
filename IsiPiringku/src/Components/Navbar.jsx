369b9cdcfc7a5e4d1575fe7ed404d936538e2ca5
arinanabila <areenrhmania@gmail.com>
2025-07-08 09:15:53 +0700

    update login

--- IsiPiringku/src/Components/Navbar.jsx
+++ IsiPiringku/src/Components/Navbar.jsx
@@ -3,11 +3,11 @@ import logo from '../assets/Logo Isi Piringku Tulisan Samping 2.png';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 flex items-center justify-between h-22 px-8 py-2 bg-[#8BDF8D] border-b border-[#6FBF70] shadow-md">
-      <div>
+    <nav className="fixed top-0 left-0 w-full z-50 flex items-center h-22 px-8 py-2 bg-[#8BDF8D] border-b border-[#6FBF70] shadow-md">
+      <div className="absolute left-8">
         <img src={logo} alt="Logo" className="h-27 object-contain " />
       </div>
-      <div className="flex gap-20">
+      <div className="mx-auto flex gap-20">
         <button className="flex items-center justify-center text-white text-2xl h-10 w-27 rounded-md hover:bg-[#995B00]/40 hover:shadow hover:shadow-white transition">Home</button>
         <button 
         className="flex items-center justify-center text-white text-2xl h-10 w-27 rounded-md hover:bg-[#995B00]/40 hover:shadow hover:shadow-white transition"
@@ -19,10 +19,6 @@ const Navbar = () => {
         }}
           >Artikel
         </button>
-      </div>
-      <div className="flex gap-7">
-        <button className="bg-[#4CAF50]/40 text-[#224F24] text-2xl h-10 w-26 rounded-md font-light hover:shadow hover:shadow-white transition border border-white">Login</button>
-        <button className="bg-[#FF9800]/40 text-[#995B00] text-2xl h-10 w-26 rounded-md font-light hover:shadow hover:shadow-white transition border border-white">Sign In</button>
       </div>
     </nav>
   );
