710d1017475818572100ab86421705d677f05b76
HMNTR <kurniaputra.irham@gmail.com>
2025-07-29 00:37:06 +0700

    update font

--- IsiPiringku/src/layouts/DetailUserLayouts.jsx
+++ IsiPiringku/src/layouts/DetailUserLayouts.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function DashboardLayouts() {
     return (
-        <div className="min-h-screen w-full px-16 font-sans bg-[#F5F5F5]">
+        <div className="min-h-screen w-full px-16 bg-[#F5F5F5]">
             <NavbarDashboard />
             <main>
                 <Outlet />
--- IsiPiringku/src/layouts/MainLayout.jsx
+++ IsiPiringku/src/layouts/MainLayout.jsx
@@ -5,8 +5,8 @@ import Footer from "../Components/Footer";
 
 const MainLayout = ({ children }) => {
 	return (
-		<div className="font-sans min-h-screen">
-			
+		<div className="min-h-screen">
+
 			<Navbar />
 			<div className="pt-16">{children}</div>
 			<Footer />
